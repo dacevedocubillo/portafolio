6dec60bf77d56c03b81ba7bbe7d3f5a97ed436f9
Daniela Acevedo <dacevedocubillo@outlook.com>
2026-07-22 16:39:21 -0600

    change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portafolio/' // 👈 nombre exacto de tu repositorio
+  base: '/portafolio/' // 👈 nombre exacto de tu repositorio
 })
 
